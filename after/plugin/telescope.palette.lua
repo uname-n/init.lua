a6e9d01554b524345974f5421c317aa345780836
uname-n <git@defend.sh>
2023-04-08 10:31:57 -0700

    after

---
+++ after/plugin/telescope.palette.lua
@@ -0,0 +1,15 @@
+require('telescope').setup({
+    extensions = {
+        command_palette = {
+            {"navigation",
+                { "dir (<leader>pd)", ":lua vim.cmd.Ex()" },
+                { "harpoon.anchor (<leader>ha)", ":lua require('harpoon.mark').add_file()" },
+                { "harpoon (<leader>hh)", ":lua require('harpoon.ui').toggle_quick_menu()" },
+            },
+        }
+    }
+})
+
+require('telescope').load_extension('command_palette')
+
+vim.keymap.set("n", "<leader><leader><leader>", "<CMD>Telescope command_palette<CR>")
