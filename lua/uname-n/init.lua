17a1f21db1f428e8624c00e78b04e8c53ddea2e1
uname-n <git@defend.sh>
2023-04-12 11:08:02 -0700

    search nerdtree across all tabs

--- lua/uname-n/init.lua
+++ lua/uname-n/init.lua
@@ -2,9 +2,24 @@ require("uname-n.packer")
 require("uname-n.set")
 require("uname-n.remap")
 
+function IsNERDTreeOpen()
+    for i = 1, vim.fn.tabpagenr('$') do
+        local buflist = vim.fn.tabpagebuflist(i)
+        for _, bufnr in ipairs(buflist) do
+            local bufname = vim.api.nvim_buf_get_name(bufnr)
+            if string.match(bufname, 'NERD_tree_') then
+                return true
+            end
+        end
+    end
+    return false
+end
+
  function SetupEditor()
-    vim.cmd(':Copilot split')
-    vim.cmd(':NERDTreeFocus')
+     if not IsNERDTreeOpen() then
+         vim.cmd(':Copilot split')
+         vim.cmd(':NERDTree')
+     end
  end
 
  vim.cmd('command! SetupEditor :lua SetupEditor()')
